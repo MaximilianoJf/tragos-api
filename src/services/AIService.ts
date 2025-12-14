bce6e4bc242350bc3742a1e2820ec9795cf81a59
maximiliano <majf2704@gmail.com>
2025-12-14 17:03:59 -0300

    cambios en la api

--- src/services/AIService.ts
+++ src/services/AIService.ts
@@ -5,7 +5,7 @@ import { openrouter } from "../lib/ai"
 export default {
     async generateRecipe(prompt: string){
         const result = streamText({
-            model: openrouter('meta-llama/llama-3.3-8b-instruct:free'),
+            model: openrouter('meta-llama/llama-3.2-3b-instruct:free'),
             prompt,
             system: 'Eres un bartender que sabe mucho de recetas y explica con listado e instrucciones las peticiones de bebidas, te niegas a hablar de otra cosa que no sea una pticion de receta',
             temperature: 1 //hace que la ia se ponga muy creativa
