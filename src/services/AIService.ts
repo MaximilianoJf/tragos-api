2f1a682196927662f2a43bc70cb2413d0ebe7df6
maximiliano <majf2704@gmail.com>
2025-12-14 18:33:03 -0300

    chancgue model aiservice

--- src/services/AIService.ts
+++ src/services/AIService.ts
@@ -8,10 +8,10 @@ export default {
             model: openrouter('meta-llama/llama-3.2-3b-instruct:free'),
             prompt,
             system: 'Eres un bartender que sabe mucho de recetas y explica con listado e instrucciones las peticiones de bebidas, te niegas a hablar de otra cosa que no sea una pticion de receta',
-            temperature: 1 //hace que la ia se ponga muy creativa
+            temperature: 1 
         })
 
-        
+
         return result.textStream
     }
 }
